fe5da35206fa8cdd70982416619bd2c022dd55fa
Evaldo Cardoso <evaldorcardoso@outlook.com>
2022-04-23 12:51:56 -0300

    seed: updating password generation for admin User

--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -1,13 +1,18 @@
 import { define } from 'typeorm-seeding';
 import { User } from '../users/entities/user.entity';
+import * as bcrypt from 'bcrypt';
 
 define(User, () => {
   const user = new User();
   user.name = 'Admin';
   user.email = 'admin@email.com';
-  user.password = '@321Abc';
+  //wait a few seconds
+  user.salt = bcrypt.genSaltSync();
+  // user.salt = bcrypt.genSalt();
+  //wait a few seconds
+  user.password = bcrypt.hashSync('@321Abc', user.salt);
+  // user.password = bcrypt.hash('@321Abc', user.salt);
   user.role = 'ADMIN';
-  user.salt = '';
   user.is_active = true;
   return user;
 });
