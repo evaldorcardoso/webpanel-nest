e9b9a6bfa4e3e94ac3d5050b142e2b63dcde6334
Evaldo Cardoso <evaldorcardoso@outlook.com>
2022-04-23 12:52:37 -0300

    seed: updating password generation for admin User

--- src/factories/user.factory.ts
+++ src/factories/user.factory.ts
@@ -6,12 +6,8 @@ define(User, () => {
   const user = new User();
   user.name = 'Admin';
   user.email = 'admin@email.com';
-  //wait a few seconds
   user.salt = bcrypt.genSaltSync();
-  // user.salt = bcrypt.genSalt();
-  //wait a few seconds
   user.password = bcrypt.hashSync('@321Abc', user.salt);
-  // user.password = bcrypt.hash('@321Abc', user.salt);
   user.role = 'ADMIN';
   user.is_active = true;
   return user;
